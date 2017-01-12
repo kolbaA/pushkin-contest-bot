0a8504c17d523e78512db76c184be4d3f6871cb0
kolbaA <kolba.mail.by284@gmail.com>
2017-01-12 20:25:30 +0300

    some deploy settings

--- Capfile
+++ Capfile
@@ -1,13 +1,15 @@
 # Load DSL and set up stages
 require "capistrano/setup"
+require "capistrano/deploy"
 
 require 'capistrano/bundler'
+require 'capistrano/rails'
 require 'capistrano/rvm'
 require 'capistrano/rails/assets' # for asset handling add
 require 'capistrano/rails/migrations' # for running migrations
 require 'capistrano/puma'
 # Include default deployment tasks
-require "capistrano/deploy"
+
 
 # Load the SCM plugin appropriate to your project:
 #
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,8 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-
+set :branch, "master"
+set :rails_env, "production"
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
  server "77.244.213.7", user: "deploy", roles: %w{pushkin_production}
