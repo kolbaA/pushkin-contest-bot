82173e3a991d5bd6f09c7abf8a45ac37b30c1b7b
kolbaA <kolba.mail.by284@gmail.com>
2017-01-13 20:42:03 +0300

    let it work

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,24 +7,23 @@ server '77.244.213.7',  primary: true
 set :application, 'pushkin-contest-bot'
 set :repo_url, 'git@github.com:kolbaA/pushkin-contest-bot.git'  # Edit this to match your repository
 set :branch, :master
-set :deploy_to, '/var/pain/pushkin-contest-bot'
+set :deploy_to, '/var/www/pushkin-contest-bot'
 set :pty, true
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/puma.rb}
 set :keep_releases, 5
 set :bundle_binstubs, nil
 set :rvm_ruby_version, 'ruby-2.3.1'   # Edit this to match ruby version you use
 set :stage, :production
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "/var/pain/pushkin-contest-bot/shared/tmp/pids/puma.state"
-set :puma_pid, "/var/pain/pushkin-contest-bot/shared/tmp/pids/puma.pid"
-set :puma_bind, "unix:///var/pain/pushkin-contest-bot/shared/tmp/sockets/puma.sock"    #accept array for multi-bind
-set :puma_conf, "/var/pain/pushkin-contest-bot/shared/puma.rb"
-set :puma_access_log, "/var/pain/pushkin-contest-bot/shared/tmp/log/puma_error.log"
-set :puma_error_log, "/var/pain/pushkin-contest-bot/shared/tmp/log/puma_access.log"
+set :puma_state, "/var/www/pushkin-contest-bot/shared/tmp/pids/puma.state"
+set :puma_pid, "/var/www/pushkin-contest-bot/shared/tmp/pids/puma.pid"
+set :puma_bind, "unix:///var/www/pushkin-contest-bot/shared/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_conf, "/var/www/pushkin-contest-bot/shared/puma.rb"
+set :puma_access_log, "/var/www/pushkin-contest-bot/shared/tmp/log/puma_error.log"
+set :puma_error_log, "/var/www/pushkin-contest-bot/shared/tmp/log/puma_access.log"
 set :puma_role, :app
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 8]
 set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
--- config/puma.rb
+++ config/puma.rb
@@ -1,47 +1,29 @@
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum, this matches the default thread size of Active Record.
-#
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-threads threads_count, threads_count
-#bind "unix:/var/www/pushkin-contest-bot/shared/tmp/sockets/puma.sock"
-# Specifies the `port` that Puma will listen on to receive requests, default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+#!/usr/bin/env puma
 
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+#environment 'production'
+daemonize false
 
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked webserver processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+pidfile '/var/www/pushkin-contest-bot/shared/tmp/pids/puma.pid'
+state_path '/var/www/pushkin-contest-bot/shared/tmp/pids/puma.state'
 
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory. If you use this option
-# you need to make sure to reconnect any threads in the `on_worker_boot`
-# block.
-#
-# preload_app!
+stdout_redirect 'log/puma.log', 'log/puma_err.log'
 
-# The code in the `on_worker_boot` will be called if you are using
-# clustered mode by specifying a number of `workers`. After each worker
-# process is booted this block will be run, if you are using `preload_app!`
-# option you will want to use this block to reconnect to any threads
-# or connections that may have been created at application boot, Ruby
-# cannot share connections between processes.
-#
-# on_worker_boot do
-#   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+# quiet
+threads 0, 16
+bind 'unix:///var/www/pushkin-contest-bot/shared/tmp/sockets/puma.sock'
+
+# on_restart do
+#   puts 'On restart...'
 # end
 
-# Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+# === Cluster mode ===
+
+# workers 2
+
+# on_worker_boot do
+#   puts 'On worker boot...'
+# end
+
+# === Puma control rack application ===
+
+activate_control_app
